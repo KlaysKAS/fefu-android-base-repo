a34540ce647b7f30664a156bcb48142873cac3ca
KlaysKAS <breakitedkirill@mail.ru>
2021-10-11 14:09:42 +1000

    Added links to text, autocompete field and fixed paddings

--- app/src/main/java/ru/fefu/activitytracker/RegistrationScreen.kt
+++ app/src/main/java/ru/fefu/activitytracker/RegistrationScreen.kt
@@ -1,12 +1,16 @@
 package ru.fefu.activitytracker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.text.SpannableString
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.ImageView
+import androidx.core.text.buildSpannedString
+import androidx.core.text.set
 
 class RegistrationScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_screen)
@@ -15,5 +19,18 @@ class RegistrationScreen : AppCompatActivity() {
         backButton.setOnClickListener {
             finish()
         }
+
+
+        val genderCompleteTextView = findViewById<AutoCompleteTextView>(R.id.GenderDropDown)
+        val genderList = arrayListOf("Male", "Female")
+        val genderAdapter = ArrayAdapter(
+            applicationContext,
+            R.layout.support_simple_spinner_dropdown_item,
+            genderList
+        )
+        genderCompleteTextView.setAdapter(genderAdapter)
+        genderCompleteTextView.threshold = 0
+
+
     }
 }
